5777a9dc7f3eb26f03398647e22630fca4a5eaff
github username <github email address>
2017-06-06 14:52:19 -0400

    Done.

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -4,22 +4,46 @@ export default class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: "",
+      password: ""
+    };
   }
+
+  handleInput(field, event){
+    this.setState({
+      [field]: event.target.value
+    })
+  }
+
+  handleSubmit(event){
+    event.preventDefault()
+    const { username, password } = this.state
+
+    if ( !username || !password ) {
+      return
+    }
+
+
+    this.props.onSubmit({
+      username,
+      password
+    })
+}
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit.bind(this)}>
         <div>
           <label>
             Username
-            <input id="test-username" type="text" />
+            <input id="test-username" type="text" value={this.state.username} onChange={this.handleInput.bind(this, 'username')} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="test-password" type="password" />
+            <input id="test-password" type="password" value={this.state.password} onChange={this.handleInput.bind(this, 'password')} />
           </label>
         </div>
         <div>
--- components/TwitterMessage.js
+++ components/TwitterMessage.js
@@ -1,17 +1,28 @@
 import React from 'react';
 
 export default class TwitterMessage extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.state = {};
+    this.state = {
+      characters: props.maxChars
+    };
+  }
+
+  handleChange(){
+    this.setState(function( previousState ){
+      return {
+        characters: --previousState.characters
+      }
+    })
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input type='text' value="f" onChange={this.handleChange.bind(this)}/>
+        <p>Characters remaining: {this.state.characters}</p>
       </div>
     );
   }
